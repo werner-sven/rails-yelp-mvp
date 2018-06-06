6470f9437e3b5d9d8fff77ea9bc4cfac7e5152ca
Sven Werner <vegascowboy@web.de>
2018-06-06 15:31:52 +0200

    Build all minimum requirements of the website without any design

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :restaurant_id, presence: true
+  validates :restaurant_id,   presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
-  validates :rating, :type => Integer
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
 end
